5dc592795c5c14d89426ab407cf3186578b8ffbf
Shun Nishitsuji <asuforcegt@hotmail.co.jp>
2016-01-17 14:52:59 +0900

    maze.txtをシムリンクに変更

--- ruby/maze.rb
+++ ruby/maze.rb
@@ -1,32 +1,36 @@
-require './route'
-
 class Maze
 
   def initialize
-    @current_step = 1
-    @count_field = Array.new($field.length).map{Array.new($field[0].length, -1)}
+    @field = Array.new
+    File.open('maze.txt') do |file|
+      file.each_line do |line|
+        @field << line
+      end
+    end
+    @current_step = 0
+    @count_field = Array.new(@field.length).map{ Array.new(@field[0].length, -1) }
     @start_pos = Hash.new
     @goal_pos = Hash.new
     @loop_flag = true
+  end
 
+  def search_root
     set_start
 
-    check_around_start(@start_pos[:x], @start_pos[:y]-1)
-    check_around_start(@start_pos[:x]+1, @start_pos[:y])
-    check_around_start(@start_pos[:x], @start_pos[:y]+1)
-    check_around_start(@start_pos[:x]-1, @start_pos[:y])
+    fill_in_number(@start_pos[:x], @start_pos[:y]-1)
+    fill_in_number(@start_pos[:x]+1, @start_pos[:y])
+    fill_in_number(@start_pos[:x], @start_pos[:y]+1)
+    fill_in_number(@start_pos[:x]-1, @start_pos[:y])
 
-    while @loop_flag
-      scanning
-    end
+    scanning
 
     route_decision(@goal_pos[:x], @goal_pos[:y])
   end
 
   def set_start
-    for i in 0...$field.length
-      for j in 0...$field[i].length
-        if($field[i][j] == 'S')
+    for i in 0...@field.length
+      for j in 0...@field[i].length
+        if(@field[i][j] == 'S')
           @start_pos = { :x => j, :y => i }
           @count_field[i][j] = 0
           return
@@ -35,103 +39,96 @@ class Maze
     end
   end
 
-  def check_around_start(pos_x, pos_y)
-    if($field[pos_y])
+  def fill_in_number(pos_x, pos_y)
+    if(@field[pos_y])
       return if(pos_x < 0)
-      case $field[pos_y][pos_x]
+
+      case @field[pos_y][pos_x]
       when 'G'
         @loop_flag = false
         @goal_pos = { :x => pos_x, :y => pos_y }
+        @current_step += 1
         return
-      when '+'
-        @count_field[pos_y][pos_x] = 1
+      when ' '
+        if(@current_step == 0)
+          @count_field[pos_y][pos_x] = 1
+          @current_step = 1
+        else
+          @count_field[pos_y][pos_x] = @current_step + 1 if @count_field[pos_y][pos_x] == -1
+        end
       else
       end
     end
   end
 
   def scanning
-    for i in 0...$field.length
-      for j in 0...$field[i].length
-        if(@count_field[i][j] == @current_step)
-          fill_in_number(j, i-1)
-          fill_in_number(j+1, i)
-          fill_in_number(j, i+1)
-          fill_in_number(j-1, i)
+    while @loop_flag
+      for i in 0...@field.length
+        for j in 0...@field[i].length
+          if(@count_field[i][j] == @current_step)
+            fill_in_number(j, i-1)
+            fill_in_number(j+1, i)
+            fill_in_number(j, i+1)
+            fill_in_number(j-1, i)
+          end
+          return unless(@loop_flag)
         end
       end
-    end
 
-    next_step
-  end
-
-  def fill_in_number(pos_x, pos_y)
-    if($field[pos_y])
-      return if(pos_x < 0)
-      case $field[pos_y][pos_x]
-      when 'G'
-        @loop_flag = false
-        @goal_pos = { :x => pos_x, :y => pos_y }
-        return
-      when '+'
-        @count_field[pos_y][pos_x] = @current_step + 1 if @count_field[pos_y][pos_x] == -1
-      else
-      end
+      @current_step += 1
     end
   end
 
   def route_decision(pos_x, pos_y)
-
     @current_step -= 1
     return if @current_step < 1
 
     if(@count_field[pos_y-1])
       if(@count_field[pos_y-1][pos_x] == @current_step)
         route_decision(pos_x, pos_y-1)
-        $field[pos_y-1][pos_x] = '@'
+        @field[pos_y-1][pos_x] = '@'
         return
       end
     end
 
     if(@count_field[pos_y][pos_x+1] == @current_step)
       route_decision(pos_x+1, pos_y)
-      $field[pos_y][pos_x+1] = '@'
+      @field[pos_y][pos_x+1] = '@'
       return
     end
 
     if(@count_field[pos_y+1])
       if(@count_field[pos_y+1][pos_x] == @current_step)
         route_decision(pos_x, pos_y+1)
-        $field[pos_y+1][pos_x] = '@'
+        @field[pos_y+1][pos_x] = '@'
         return
       end
     end
 
     if(@count_field[pos_y][pos_x-1] == @current_step)
       route_decision(pos_x-1, pos_y)
-      $field[pos_y][pos_x-1] = '@'
+      @field[pos_y][pos_x-1] = '@'
       return
     end
 
   end
 
-  def next_step
-    @current_step += 1
-  end
-
   def print_count_field
     for i in 0..@count_field.length
-      print @count_field[i],"\n"
+      print @count_field[i], "\n"
     end
   end
 
   def print_field
-    for i in 0..$field.length
-      print $field[i],"\n"
+    for i in 0..@field.length
+      print @field[i]
     end
   end
 
 end
 
+st = Time.now
 m = Maze.new
+m.search_root
 m.print_field
+puts Time.now - st
